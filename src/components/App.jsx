6f15fa059aee634845b0f4a056a8615f81ac85b9
LidiiaZaichenko <119893988+LidiiaZaichenko@users.noreply.github.com>
2023-09-27 18:19:00 +0300

    add data render

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+import { Profile } from './Profile';
+import { Statistics } from './Statistics';
+import { FriendList } from './FriendList';
+import { TransactionHistory } from './TransactionHistory';
+import user from 'data/user.json';
+import data from 'data/data.json';
+import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Social network profile</h1>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <h1>Statistics section 1</h1>
+      <Statistics title="Upload stats" stats={data} />
+      <h1>Statistics section 2</h1>
+      <Statistics stats={data} />
+      <h1> Friend list </h1>
+      <FriendList friends={friends} />
+      <h1>Transaction history</h1>
+      <TransactionHistory transaction={transactions} />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
+// import { Profile } from 'components/Profile';
+// import { TransactionHistory } from 'components/TransactionHistory';
+// import { Statistics } from 'components/Statistics';
+// import { FriendList } from 'components/FriendList';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+/* <Profile />
+<Statistics />
+<FriendList />
+<TransactionHistory /> */
